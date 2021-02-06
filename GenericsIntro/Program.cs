de7e87f2a11487413ae03764bf2aee827d2f074a
Tansu Çağla Tekin <tansu_2001@hotmail.com>
2021-02-06 22:55:29 +0300

    06.02.2021

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string> ();
+            isimler.Add("Engin");
+
+            Console.WriteLine(isimler.Length);
+
+            isimler.Add("Kerem");
+
+            Console.WriteLine(isimler.Length);
+
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
+
+            //List<string> liste = new List<string>();
+            //Console.WriteLine(liste.Count);
+            //count = eleman sayısı
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
