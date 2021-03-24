c4890e2fe2152c02e2b124202d462a19226bb15b
Tansu Çağla Tekin <tansu_2001@hotmail.com>
2021-03-24 15:31:10 +0300

    24.03.2021

---
+++ OOP3/FilebaseLoggerService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OOP3
+{
+    class FilebaseLoggerService : ILoggerService
+    {
+        public void Log()
+        {
+            Console.WriteLine("Dosyaya loglandı");
+        }
+    }
+}
+
+
